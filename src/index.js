4153fbeef4c06d0774a01f93532c10eac51fc5e0
surrajj20 <shah.suraj1200@gmail.com>
2020-05-06 17:34:56 +0530

    Brodcasting event & sharing location added

--- src/index.js
+++ src/index.js
@@ -16,9 +16,19 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `http://google.com/maps/?q=${coords.latitude},${coords.longitude}`)
     })
 })
 
