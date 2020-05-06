a2b4975f11b41a12b94ac65a9d6f92d4d53638ae
surrajj20 <shah.suraj1200@gmail.com>
2020-05-06 15:47:48 +0530

    socket event handling

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,23 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = document.querySelector('input').value
+
+    socket.emit('sendMessage', message)
+
+})
+
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated!', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
--- src/index.js
+++ src/index.js
@@ -12,8 +12,27 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+// let count = 0
+
+// server(emit) -> client (receive) - countUpdated
+// client(emit) -> server (receive) - increment
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    // socket.emit('countUpdated', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     //socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+    // })
 })
 
 server.listen(port, () => {
