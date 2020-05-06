ae7364375942800e76621499a3bca3f33563bbee
surrajj20 <shah.suraj1200@gmail.com>
2020-05-06 18:15:48 +0530

    Acknowledgements setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,8 +19,15 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
 
     })
 
@@ -27,8 +35,9 @@ io.on('connection', (socket) => {
         io.emit('message', 'A user has left!')
     })
 
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('message', `http://google.com/maps/?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 })
 
