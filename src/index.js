b6373f5b8fd5e4eafd777483dcb79f6213e0d992
surrajj20 <shah.suraj1200@gmail.com>
2020-05-07 14:44:49 +0530

    rendering users list to sidebar

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ io.on('connection', (socket) => {
 
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUserInRoom(user.room)
+        })
 
         callback()
     })
@@ -58,6 +62,10 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUserInRoom(user.room)
+            })
         }
     })
 })
